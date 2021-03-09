42c91e7846aaf57109836040076d205b3998d1d5
gzhao9 <74684732+gzhao9@users.noreply.github.com>
2021-03-10 06:13:05 +0800

    Update HW04_Gengwu_Zhao.py

--- HW04_Gengwu_Zhao.py
+++ HW04_Gengwu_Zhao.py
@@ -17,10 +17,6 @@ class gituser:
                 print(e)
                 sys.exit()
 
-    def Print_info(self):
-        for i,j in self._repo.items():
-             print(f"Repo: {i}; Number of commits: {j}")
-    
     def get_repo_list(self):
         return self._repo.keys()
     
@@ -29,12 +25,3 @@ class gituser:
             return self._repo[name]
         else:
             return -1
-
-def main():
-    userid=input("please enter the user ID of github:")
-    User1=gituser("gzhao9")
-    User1.Print_info()
-
-#if __name__ == '__main__':
-    #main()
-    
